d3f1aad3a95ac6d9ec88a90117d1305c4f31fcc9
alexander <aleksandr.tyagunov@gmail.com>
2021-03-27 18:44:10 +0700

    amended

--- JSON/JSON_exercise1.py
+++ JSON/JSON_exercise1.py
@@ -1,16 +1,11 @@
 import json
 
 
-with open('wikidata_1000.json') as f:
-    wordsList = []
+with open('wikidata_1000.json') as f, open("dict.json", 'w') as d:
     for i in f:
-        wordsDict = json.loads(i)
-        wordsList.append(wordsDict)
-
-
-    for word in wordsList:
+        word = (json.loads(i))
         if 'description' in word.keys():
-            print(json.dumps({word["label"]['value']: word["description"]['value']}, sort_keys=True,ensure_ascii=False))
+            json.dump({word["label"]['value']: word["description"]['value']}, d, indent=2, sort_keys=True,ensure_ascii=False)
 
         else:
-            print(json.dumps({word["label"]['value']: 'None'}, sort_keys=True, ensure_ascii=False))
+            json.dump({word["label"]['value']: None}, d, indent=2, sort_keys=True, ensure_ascii=False)
--- JSON/JSON_exercise2.py
+++ JSON/JSON_exercise2.py
@@ -1,7 +1,6 @@
 import json
-import sys
 
-with open('RomeoAndJuliet.json') as f, open("result.json", 'w') as sys.stdout:
+with open('RomeoAndJuliet.json') as f, open("result.json", 'w') as res:
     data = json.load(f)
     for acts in data['acts']:
         for scenes in acts['scenes']:
@@ -9,6 +8,7 @@ with open('RomeoAndJuliet.json') as f, open("result.json", 'w') as sys.stdout:
             for actions in scenes['action']:
                 if (actions['character']) not in characters:
                     characters.append(actions['character'])
-            print(characters)
-
+            json.dump(characters, res)
+            res.write('\n')
+#Я не понимаю, как записать файл методом json.dumps...
 
