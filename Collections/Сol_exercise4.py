50fcac14ea9f73846ac655728d3691fb5b25bcf6
alexander <aleksandr.tyagunov@gmail.com>
2021-06-04 22:25:17 +0700

    amended

--- Collections/Сol_exercise4.py
+++ Collections/Сol_exercise4.py
@@ -1,20 +1,46 @@
 import collections as c
 import csv
+from functools import cmp_to_key
+from contextlib import redirect_stdout
 
-with open('/home/alexander/PycharmProjects/python_practice/CSV/stage3_test.csv') as csv_in, open("result.csv", 'w', newline='') as out_csv:
-    csv_reader = csv.DictReader(csv_in)
-    d = {}
+
+def compare_adverts(a, b):
+    return float(a[len(a) - 1]) - float(b[len(b) - 1])
+
+with open('/home/alexander/PycharmProjects/python_practice/CSV/stage3_test.csv') as csv_in, open("result.txt", 'w', newline='') as f:
+    csv_reader = csv.reader(csv_in)
+    next(csv_reader)
+    list = []
     for row in csv_reader:
-       k, v = row['Title'], row['Price']
-       d[k] = v
+        k, v = row[2], row[len(row) - 1]
+        list.append([k, v])
+
+    min_max = sorted(list, key=cmp_to_key(compare_adverts))
+    max_min = sorted(list, key=cmp_to_key(compare_adverts), reverse=True)
+    min_max_dict = c.OrderedDict({})
+    max_min_dict = c.OrderedDict({})
+    for row in min_max:
+        min_max_dict[row[0]] = row[len(row) - 1]
+
+    for row in max_min:
+        max_min_dict[row[0]] = row[len(row) - 1]
+
+    with redirect_stdout(f):
+
+        print("Цены по возрастанию")
+        for row in min_max_dict.items():
+            print("Заголовок: ", row[0], "Стоимость: ",  row[1])
 
 
-    min_to_max = c.OrderedDict(sorted(d.items(), key=lambda t: t[1]))
-    max_to_min = c.OrderedDict(sorted(d.items(), key=lambda t: t[1], reverse=True))
+        print("Цены по убыванию")
+        for row in max_min_dict.items():
+            print("Заголовок: ", row[0], "Стоимость: ", row[1])
 
-    writer = csv.writer(out_csv)
-    for key, value in max_to_min.items():
-        writer.writerow([key, value])
+
+
+
+
+
 
 
 
